d9cfd39e87715cd264f8836e79f1e7b9934bd173
aldrin.reyes <r.aldrin1209@gmail.com>
2019-12-17 21:58:24 -0800

    "refractor recycler view"

--- app/src/main/java/com/aldrin/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/aldrin/coderswag/Controller/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aldrin.coderswag.Adapters.CategoryAdapter
+import com.aldrin.coderswag.Adapters.CategoryRecycleAdapter
 import com.aldrin.coderswag.Model.Category
 import com.aldrin.coderswag.R
 import com.aldrin.coderswag.Services.DataService
@@ -13,13 +15,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
